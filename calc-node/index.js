ea81ebe1ca4f8fa967d19696cdb8ac26659b6e05
Anastasiia Kholod <121211989+asyaruda@users.noreply.github.com>
2023-08-28 19:18:40 +0300

    Add files via upload

---
+++ calc-node/index.js
@@ -0,0 +1,22 @@
+function addCount(a, b) {
+  return a + b
+}
+
+function subCount(a, b) {
+  return a - b
+}
+
+function multCount(a, b) {
+  return a * b
+}
+
+function divCount(a, b) {
+  return a / b
+}
+
+
+
+module.exports.addCount = addCount
+module.exports.subCount = subCount
+module.exports.multCount = multCount
+module.exports.divCount = divCount
