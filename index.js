280631c0748fbc30ac93717c767107a74596bbb3
Anastasiia Kholod <121211989+asyaruda@users.noreply.github.com>
2023-08-28 19:18:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const { addCount,
+        subCount,
+        multCount,
+        divCount } = require('./calc-node/index.js')
+
+       console.log(addCount(0, 2))
+       console.log(subCount(12, 5))
+       console.log(multCount(7, 4))
+       console.log(divCount(13, 2))
