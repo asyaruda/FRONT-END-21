31ce54cddb42d8dac2699117f0c91969e8f51583
Anastasiia Kholod <121211989+asyaruda@users.noreply.github.com>
2023-08-30 20:58:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { addCount,
-        subCount,
-        multCount,
-        divCount } = require('./calc-node/index.js')
+const { addCount } = require('./calc-node/addCount.js')
+const { subCount } = require('./calc-node/subCount.js')
+const { multCount } = require('./calc-node/multCount.js')
+const { divCount } = require('./calc-node/divCount.js')
 
-       console.log(addCount(0, 2))
-       console.log(subCount(12, 5))
-       console.log(multCount(7, 4))
-       console.log(divCount(13, 2))
+ console.log(addCount(0, 2))
+ console.log(subCount(12, 25))
+ console.log(multCount(7, 0.0015))
+ console.log(divCount(13, 2))
